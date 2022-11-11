dd31ec6a5c8cb993c7ffd6e0f240cc38fabd5f1a
Evan K <30984495+Ekinn7188@users.noreply.github.com>
2022-11-11 15:26:43 -0800

    Time: 18 ms (84.80%), Space: 12 MB (13.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //           <value, index>
+        unordered_map<int, int> possible_solutions;
         vector<int> solution;
         
-        for (int i = 0; i < nums.size() - 1; i++) {
-            for (int j = i+1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    solution.push_back(i);
-                    solution.push_back(j);
-                    break;
-                }
+        for (int i = 0; i < nums.size(); i++) {
+            // If target-nums[i] exists, then it's been iterated past and the solution is in the map.
+            unordered_map<int,int>::iterator key = possible_solutions.find(target-nums[i]);
+            if (key != possible_solutions.end()) {
+                // possible_solutions[key] gives the index
+                solution.push_back(possible_solutions[key->first]);
+                solution.push_back(i);
             }
+            
+            possible_solutions[nums[i]] = i;
         }
         
         return solution;
